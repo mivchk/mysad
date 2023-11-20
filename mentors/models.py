3a91325e9f2451f91a7b93ee2904523f520dbb6d
m.ivchik <misas20202@gmail.com>
2023-11-20 22:52:55 +0300

    добавил класс дети

--- mentors/models.py
+++ mentors/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Kids(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    group = models.CharField(max_length=100)
