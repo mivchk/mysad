d9bb321d986c547dc94b3b261e60662b5f2ce811
m.ivchik <misas20202@gmail.com>
2023-11-24 00:40:12 +0300

    Прописал все ключевые модели, добавил новый модуль в requirements

--- mentors/models.py
+++ mentors/models.py
@@ -1,3 +1,40 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Mentor(models.Model):
+    mentor_id = models.AutoField(primary_key=True)
+    mentor_name = models.CharField(max_length=100)
+    professional_grade = models.CharField(max_length=100)
+    username = models.CharField(max_length=20, default=models.SET_NULL)
+    password = models.CharField(max_length=20, default=models.SET_NULL)
+
+
+class Group(models.Model):
+    group_id = models.AutoField(primary_key=True)
+    mentor_id = models.ForeignKey('Mentor', on_delete=models.CASCADE)
+    grade = models.IntegerField()
+
+
+class Reminders(models.Model):
+    reminder_id = models.AutoField(primary_key=True)
+    message = models.CharField(max_length=255)
+    deadline = models.DateField()
+
+
+class Attendance(models.Model):
+    attendance_id = models.AutoField(primary_key=True)
+    today_date = models.DateField()
+    child_id = models.ForeignKey('Child', on_delete=models.CASCADE)
+    is_arrived = models.BooleanField(default=False)
+    mentor_id = models.ForeignKey('Mentor', on_delete=models.CASCADE)
+
+
+class Child(models.Model):
+    child_id = models.AutoField(primary_key=True)
+    child_name = models.CharField(max_length=40)
+    birth_date = models.DateField()
+    group_id = models.ForeignKey('Group', on_delete=models.CASCADE)
+
+
+
