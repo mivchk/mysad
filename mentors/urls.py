836ce1ca3e80e8f1943a999eb33515aac4dffd30
m.ivchik <m.ivchik@bftcom.com>
2024-01-08 19:11:26 +0300

    Последняя рабочая версия

--- mentors/urls.py
+++ mentors/urls.py
@@ -5,8 +5,4 @@ urlpatterns = [
     path('reminder/', reminder_view, name='m_reminder'),
     path('get_group/', get_group_view, name='get_group'),
     path('child_list/<int:group_number>/', mark_attendance_view, name='get_child'),
-    # path('att/<int:child_id>/', attendance_set, name='attendance'),
-    # path('child_list/<int:group_number>/', get_child, name='get_child'),
-    # path('hello_prep/<prepod>/', show_prepod),
-    # path('navbar/', navbar),
 ]
